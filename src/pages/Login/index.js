15d300134c48ae57c031dbc2733540f9a9aaaa84
levyhnunes <levyhcosta@gmail.com>
2020-05-28 17:07:50 -0300

    Sign-In Google

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Button, AsyncStorage } from "react-native";
+import * as Google from 'expo-google-app-auth';
 
 import { AuthContext } from "../../services/context";
 // import { Container } from './styles';
@@ -8,10 +9,30 @@ const Login = () => {
 
   const { signIn } = React.useContext(AuthContext);
 
-  const logar = () => {
+  async function signInWithGoogleAsync() {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: YOUR_CLIENT_ID_HERE,
+        // iosClientId: YOUR_CLIENT_ID_HERE,
+        scopes: ['profile', 'email'],
+      });
+
+      console.log(JSON.stringify(result));
+  
+      if (result.type === 'success') {
+        logar(result.accessToken, result.user.email, result.user.name);
+      } else {
+        console.log('cancelado');
+      }
+    } catch (e) {
+      console.log('error', e);
+    }
+  }
+
+  const logar = (accessToken, email, name) => {
     AsyncStorage.setItem(
       "userData",
-      JSON.stringify({ userName: "Usuario Teste", token: "hdehfkenioi312" })
+      JSON.stringify({ userName: name, userEmail: email, token: accessToken })
     );
 
     signIn();
@@ -19,7 +40,7 @@ const Login = () => {
 
   return (
     <View>
-      <Button title="ENTRAR" onPress={logar} />
+      <Button title="ENTRAR" onPress={signInWithGoogleAsync} />
     </View>
   );
 };
